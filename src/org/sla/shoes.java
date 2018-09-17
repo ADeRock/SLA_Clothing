9ee960cb4b8828ecece9544c950d07ad6c263bae
andrewrodebaugh <aquahydration@gmail.com>
2018-09-17 14:55:34 -0400

    Added shoes

--- src/org/sla/shoes.java
+++ src/org/sla/shoes.java
@@ -1,4 +1,41 @@
 package org.sla;
 
 public class shoes {
+    int size;
+    String style;
+    int price;
+    boolean colorful;
+    boolean purchased = false;
+
+    int getSize() {
+        return size;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    void describe() {
+        System.out.println("This size " + getSize() + " shoe is $" + getPrice());
+
+        if (colorful) {
+            System.out.println("This is very colorful.");
+        }
+
+        System.out.println("The style is " + style);
+
+        if (purchased) {
+            System.out.println("This item has been bought.");
+        } else {
+            System.out.println("This item has not been bought.");
+        }
+    }
+
+    void purchase() {
+        if (!purchased) {
+            System.out.println("Thank you!");
+            purchased = true;
+        }
+    }
+
 }
